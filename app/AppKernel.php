e88e72ac364fc96addf228b37175d7a57e1673d7
Thibaut MONIER <thibaut.monier@gmail.com>
2014-07-08 17:12:39 +0200

    update code formatting

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -20,6 +20,10 @@ class AppKernel extends Kernel
             new Knp\RadBundle\KnpRadBundle(),
             new Ob\HighchartsBundle\ObHighchartsBundle(),
             new App\App(),
+
+            new Admingenerator\GeneratorBundle\AdmingeneratorGeneratorBundle(),
+            new Knp\Bundle\MenuBundle\KnpMenuBundle(),
+            new WhiteOctober\PagerfantaBundle\WhiteOctoberPagerfantaBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
